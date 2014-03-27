031492a2ea474643530ed124861a54abebc22130
Splintex <eugene.godin@gmail.com>
2014-03-27 17:46:09 +0200

    upd

--- js/common.js
+++ js/common.js
@@ -101,17 +101,17 @@ $(document).ready(function() {
     });
 
     $(".js-tab").tabs({
-      beforeLoad: function( event, ui ) {
-        ui.jqXHR.error(function() {
-          ui.panel.html(
-            "Couldn't load this tab. We'll try to fix this as soon as possible. " +
-            "If this wouldn't be a demo." );
-        });
-      },
+        beforeLoad: function( event, ui ) {
+            ui.jqXHR.error(function() {
+              ui.panel.html(
+                "Couldn't load this tab. We'll try to fix this as soon as possible. " +
+                "If this wouldn't be a demo." );
+            });
+        },
         activate: function(event, ui) { 
             window.location.hash=ui.newPanel.selector; 
         },
-        hide: function(event, ui) { effect: "explode", duration: 1000 } 
+        
     });
 
 

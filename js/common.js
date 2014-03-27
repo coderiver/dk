511025f9a7f81429165a5734c9344d303ce93fdb
Splintex <eugene.godin@gmail.com>
2014-03-27 13:00:29 +0200

    upd

--- js/common.js
+++ js/common.js
@@ -71,4 +71,26 @@ $(document).ready(function() {
         var val = $(this).val();
         $(this).parent().find(".js-input-file-text").text(val);
     });
+
+
+    function tab() {
+       $(".js-tab").each(function(){
+            var tab_link = $(this).find("a");
+            var tab_item = $(this).find("li");
+            var tab_cont = $(this).parents(".js-tab-group").find(".js-tab-cont");
+            tab_cont.hide();
+            tab_item.first().addClass("is-active");
+            $(this).parents(".js-tab-group").find(".js-tab1").show();
+            tab_link.on("click", function() {
+                var index = $(this).attr("href");
+                tab_item.removeClass("is-active");
+                $(this).parent().addClass("is-active");
+                tab_cont.hide();
+                $(this).parents(".js-tab-group").find("."+index).show();
+                return false;
+          });
+       });
+    }
+    tab();
+
 });
